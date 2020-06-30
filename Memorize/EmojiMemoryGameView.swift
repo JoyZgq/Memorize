fb16a7629a5b4cd9891acf6d4339d2ad73c70218
JoyZgq <neo.work@foxmail.com>
2020-06-30 22:48:30 +0800

    add grid

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -12,31 +12,44 @@ struct EmojiMemoryGameView: View {
      @ObservedObject var viewModel :EmojiMemoryGame
     
     var body: some View {
-    return HStack() {
-        ForEach(viewModel.cards){
+        Grid(viewModel.cards){
             card in CardView(card: card).onTapGesture {
                 self.viewModel.choose(card: card)
             }
+        .padding(5)
         }
+        .padding()
+        .foregroundColor(Color.orange)
     }
-    .padding()
-    .foregroundColor(Color.orange)
-    .font(Font.largeTitle)
-    }
- }
+}
 
 struct CardView:View{
     var card:MemoryGame<String>.Card
     var body:some View{
-        ZStack{
-            if card.isFaceUp{
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth:3)
-                Text(card.content)
-            }else{
-                RoundedRectangle(cornerRadius: 10.0).fill()
-            }
+        GeometryReader {
+            geometry in
+            self.body(for:geometry.size)
         }
+    }
+    
+    func body(for size:CGSize)->some View{
+            ZStack{
+                if card.isFaceUp{
+                    RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                    RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth:edgeLineWidth)
+                    Text(card.content)
+                }else{
+                    RoundedRectangle(cornerRadius: cornerRadius).fill()
+                }
+            }
+            .font(Font.system(size:fontSize(for: size)))
+    }
+    let cornerRadius:CGFloat=10.0
+    let edgeLineWidth:CGFloat=3
+//    let fontScaleFactor:CGFloat=0.75
+    
+    func fontSize(for size:CGSize) -> CGFloat {
+        min(size.width, size.height*0.75)
     }
 }
 
